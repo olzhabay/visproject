006fca6758d508c1f8cc482732f97daa1dec2c76
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2017-11-03 16:13:21 +0900

    modelsredefined

--- app/models.py
+++ app/models.py
@@ -8,13 +8,13 @@ from django.db import models
 class Node(models.Model):
 	name = models.CharField(max_length=200)
 	cpu_cores = models.IntegerField()
-	cpu_cores_usage = models.IntegerField()
+	cpu_cores_usage = models.IntegerField(default=0)
 	ram_amount = models.IntegerField()
-	ram_amount_usage = models.IntegerField()
+	ram_amount_usage = models.IntegerField(default=0)
 	disk_write_max = models.IntegerField()
-	disk_write_usage = models.IntegerField()
+	disk_write_usage = models.IntegerField(default=0)
 	disk_read_max = models.IntegerField()
-	disk_read_usage = models.IntegerField()
+	disk_read_usage = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.name
@@ -22,8 +22,8 @@ class Node(models.Model):
 class Application(models.Model):
 	name = models.CharField(max_length=200)
 	state = models.CharField(max_length=200)
-	started = models.DateTimeField()
-	finished = models.DateTimeField()
+	started = models.BigIntegerField()
+	finished = models.BigIntegerField(blank=True, null=True)
 
 	def __str__(self):
 		return self.name
@@ -34,8 +34,8 @@ class Container(models.Model):
 	type = models.CharField(max_length=200)
 	app_id = models.ForeignKey(Node)
 	node_id = models.ForeignKey(Application)
-	started = models.DateTimeField()
-	finished = models.DateTimeField()
+	started = models.BigIntegerField()
+	finished = models.BigIntegerField(blank=True, null=True)
 	vcore_allocated = models.IntegerField()
 	ram_allocated = models.IntegerField()
 
@@ -46,7 +46,7 @@ class Metric(models.Model):
 	container_id = models.ForeignKey(Container)
 	app_id = models.ForeignKey(Application)
 	node_id = models.ForeignKey(Node)
-	time = models.DateTimeField()
+	time = models.BigIntegerField()
 	cpu_usage = models.IntegerField()
 	ram_usage = models.IntegerField()
 	disk_read = models.IntegerField()
